dac633ca8c16fdb5115f0827d6c5446947644e26
avexray <21966871+avexray@users.noreply.github.com>
2026-09-16 20:58:15 +0500

    Add composer and autoload

---
+++ app/Engine.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Engine
+{
+    public function run(): void
+    {
+        echo 'hello';
+    }
+}
---
+++ app/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$engine = new \App\Engine();
+
+$engine->run();
