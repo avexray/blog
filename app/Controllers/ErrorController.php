a7ecd74d273e0e5658f09d2d25ee2f182326a032
avexray <21966871+avexray@users.noreply.github.com>
2026-09-17 22:39:57 +0500

    Add error response code

--- app/Controllers/ErrorController.php
+++ app/Controllers/ErrorController.php
@@ -6,6 +6,7 @@ class ErrorController extends Controller
 {
     public function error(string $message, int $code): void
     {
+        http_response_code($code);
         $this->renderer->view('error.tpl', [
             'code' => $code,
             'message' => $message
